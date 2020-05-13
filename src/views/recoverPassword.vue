5d40a6514758093132c5513850f4ae55fb59effc
vanessanadal <vvnadal.17@est.ucab.edu.ve>
2020-05-13 14:32:47 -0400

    FASE1-009: Se añadio codigo de internacionalizacion y validaciones

--- src/views/recoverPassword.vue
+++ src/views/recoverPassword.vue
@@ -2,8 +2,7 @@
   <v-container fluid class="bg" >
 
         <v-row no gutters class="hidden-sm-and-down"> 
-            <v-col cols="12" sm="1"> <v-icon class="mr-1" style="float: left;">mdi-arrow-left</v-icon> <h4>Go back</h4></v-col> 
-             <v-col ></v-col>
+        <v-col cols="12" sm="1"> <a style="color: gray;" @click="changePage('Home')"><v-icon class="mr-1" style="float: left;">mdi-arrow-left</v-icon> <h4>{{goBack}}</h4></a></v-col>              <v-col ></v-col>
         </v-row> 
         
         <v-row >
@@ -20,10 +19,10 @@
                     
                     <v-row> 
                         <v-col> 
-                            <v-icon class="hidden-md-and-up" style="float: left;">mdi-arrow-left</v-icon> 
+                            <a style="color: gray;" @click="changePage('Home')"><v-icon class="hidden-md-and-up" style="float: left;">mdi-arrow-left</v-icon> </a>
                         </v-col>
                         <v-col cols="12" sm="8"> 
-                            <h3>Recover password</h3>
+                            <h3>{{titleRecover}}</h3>
                         </v-col> 
                         <v-col> 
                         </v-col>  
@@ -32,11 +31,11 @@
                     <v-row> 
                         
                         <v-col> 
-                        <h5>Fill in your email and we'll send you an email to reset your password</h5>
+                        <h5>{{description}}</h5>
                         </v-col> 
                         
                     </v-row> 
-
+                <v-form ref="form">
                     <v-row> 
                         <v-col cols="12" sm="1">  
                         <v-icon class="mt-5" pa-0>mdi-account</v-icon> 
@@ -46,7 +45,7 @@
                         <v-text-field
                             v-model="email"
                             :rules="emailRules"
-                            label="E-mail"
+                            :label="emailForm"
                             required
                         ></v-text-field>
                         </v-col>
@@ -57,13 +56,13 @@
                         </v-col>
 
                         <v-col> 
-                            <v-btn rounded color="#a9ff4d" dark>Recover password</v-btn>
+                            <v-btn rounded color="#a9ff4d" dark>{{titleRecover}}</v-btn>
                         </v-col>
 
                         <v-col> 
                         </v-col>
                     </v-row> 
-
+                </v-form>
                     <v-row> 
                         <v-col> 
                         </v-col>
@@ -97,11 +96,125 @@
             <v-col> 
             </v-col>     
         </v-row> 
+
+        <v-snackbar v-model="snackbar" top:timeout="timeout" color="success">
+            {{snack1}}
+            <v-btn dark text @click="snackbar=false">{{close}}</v-btn>
+        </v-snackbar>
+        <v-snackbar v-model="snackbarError" top:timeout="timeout" color="error">
+            {{snack2}}
+             <v-btn dark text @click="snackbarError=false">{{close}}</v-btn>
+        </v-snackbar>
+
     </v-container>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+
+import Vue from "vue";
+import Component from "vue-class-component";
+import {Watch} from "vue-property-decorator";
+
+@Component({})
+export default class recoverPassword extends Vue {
+
+    email = "";
+                
+    
+    //////Variables estaticas/////
+
+                titleRecover= "Recover password"
+                description= "Fill in your email and we'll send you an email to reset your password"
+                emailForm= "E-mail"
+                goBack= "Go back"
+                snack1 = "Your password has been sended."
+                snack2 = "Please fill in your email"
+                snack3 = "Please confirm password correctly"
+                close = "Close"
+
+    //////Fin variables estaticas//////
+
+  
+    snackbar = false;
+    snackbarError = false;
+    timeout =7000;
+
+    /*searchRoute() {
+        if (this.$refs.form.validate()&&this.password2===this.user.password){
+             console.log(this.user);
+            this.$store
+            .dispatch("user/createUserRoute", this.user)
+            .then(() => {
+                //this.cosasdelaBD = this.$store.state.example.route;
+                console.log(this.user);
+                this.snackbar=true;
+                this.changePage('Home');
+        });}   else if (this.password2!==this.user.password){
+                    console.log("Datos no validos");
+                    this.snackbarPassword=true;
+                 }  else {
+                            console.log("Datos no validos");
+                            this.snackbarError=true;
+                         }
+                } 
+    */
+
+      changePage(link: string) {
+         this.$router.push({ name: link });
+  }
+
+    rules: {} = {
+        required: (value: string) =>
+        (!!value && value !== "" && value !== undefined) || "Required",
+        passwordRules: [(v: string) =>!!v || "Password is required"],
+        emailRules: [ 
+            (v:string) => !!v || "E-mail is required",
+            (v:string) => /.+@.+\..+/.test(v) || "E-mail is required",
+        ],
+     
+    };
+
+    $refs!: {
+        form:any;
+    };
+
+    //////Internationalization//////
+
+    get translator() {
+    return this.$store.state.translate.languageTexts;
+  }
+
+  @Watch("translator")
+  translate() {
+    this.translator
+      .filter(
+        (term: { context: string; name: string; translation: string }) => {
+          return term.context == "recoverPassword" || term.context == "general";
+        }
+      )
+      .forEach(
+        (term: { context: string; name: string; translation: string }) => {
+          if (term.name == "recoverPasswordTitle") {
+            this.titleRecover = term.translation;
+          } else if (term.name == "recoverPasswordDescription") {
+            this.description= term.translation;
+          } else if (term.name == "recoverPasswordEmail") {
+            this.emailForm = term.translation;
+          } else if (term.name == "recoverPasswordSnack1") {
+            this.snack1 = term.translation;
+          } else if (term.name == "recoverPasswordSnack2") {
+            this.snack2 = term.translation;
+          } else if (term.name == "srecoverPasswordnack3") {
+            this.snack3 = term.translation;
+          } else if (term.name == "generalGoBack") {
+            this.snack3 = term.translation;
+          } else if (term.name == "generalClose") {
+            this.close = term.translation;
+          } 
+          
+        }
+      );
+  } 
 
 }
 </script>
