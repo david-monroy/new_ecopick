e6b506e335110853e94830a86fde0a61a47d37a1
carolinapatino <cdpatino.17@est.ucab.edu.ve>
2020-05-26 21:59:55 -0400

    FASE1-034: Se agregó el boton de telegram

---
+++ src/components/FABTelegram.vue
@@ -0,0 +1,12 @@
+<template>
+  <span class="mb-8">
+    <v-btn color="blue" fab dark fixed bottom right edge>
+      <v-icon>mdi-telegram</v-icon>
+    </v-btn>
+  </span>
+</template>
+
+<script>
+import Vue from "vue";
+export default {};
+</script>
