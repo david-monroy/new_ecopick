5ca96a6cd0bdb7e431e03a1c4f955b431c9ccbd4
carolinapatino <cdpatino.17@est.ucab.edu.ve>
2020-05-26 22:43:08 -0400

    FASE1-034: Redirige a la página del bot

--- src/components/FABTelegram.vue
+++ src/components/FABTelegram.vue
@@ -1,12 +1,13 @@
 <template>
   <span class="mb-8">
-    <v-btn color="blue" fab dark fixed bottom right edge>
-      <v-icon>mdi-telegram</v-icon>
+    <v-btn color="blue" fab dark fixed bottom right edge href="https://t.me/MrPostel_bot">
+    <v-icon color="white" size="28px">mdi-telegram</v-icon>
     </v-btn>
   </span>
 </template>
 
 <script>
 import Vue from "vue";
-export default {};
+export default {
+}
 </script>
